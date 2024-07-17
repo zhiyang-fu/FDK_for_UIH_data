24adbf46d97929ddf8bd7b7045e5f91ae3e30bd5
Zhiyang Fu <zhiyang.fu@united-imaging.com>
2024-07-17 18:14:23 +0800

    add example script for reconstructing single/dynamic focal spot data

---
+++ FDK_axial_UIHdata.py
@@ -0,0 +1,69 @@
+# %%
+import os
+import numpy as np
+import torch
+import yaml
+
+from torch_fdk import FDK
+from Tools.imview3d import ImageSliceViewer3D
+from Tools.write_dicom import write_array_to_dicom
+from Tools.interp import torch_1d_interp
+
+import matplotlib.pyplot as plt
+plt.rcParams['image.cmap'] = 'gray'
+plt.rcParams['font.sans-serif'] = ['SimHei']
+
+# %% User Parameters
+scan_type = 'axial'
+scan_anatomy = 'head'
+preprocess_functor = 'zrebin'
+# series_uid = '1.2.156.112605.66988329187441.221003011301.9.8720.379695'
+series_uid = '1.2.156.112605.66988332599473.240410112317.9.1628.86557'
+
+data_dir = os.path.join('/data/Recon/zhiyang.fu/dat_files', scan_type, scan_anatomy)
+data_file = '{}_{}.dat'.format(series_uid, preprocess_functor)
+geo_dir = os.path.join('/data/Recon/zhiyang.fu/geometry_files', scan_type, scan_anatomy)
+geo_file = series_uid + '.yml'
+
+# %% load geo
+with open(os.path.join(geo_dir, geo_file), 'r') as f:
+    geo = yaml.load(f, yaml.SafeLoader)
+# commonly used variables
+num_angles = geo['iViewNum']
+nu, nv = geo['iDetColNum'], geo['iDetRowNum']
+du  = geo['fDetGammaEqualSpace']
+# %%
+proj = np.reshape(np.fromfile(os.path.join(data_dir, data_file), dtype=np.float32),
+               (num_angles, nv, nu))
+ImageSliceViewer3D(proj)
+# %% view truncation for overscans
+geo['pfViewAngle'] = geo['pfViewAngle'][:2400]
+proj = proj[:2400]
+# %%
+us = du * (torch.arange(nu) - nu/2 + 0.5)
+if geo['iFocalSpotNum'] == 2:
+    f = torch.tensor(np.empty_like(proj))
+    for i in range(2):
+        us_p = torch.tensor(geo['pfDetGamma'][i::2])
+        f[i::2] = torch_1d_interp(us, us_p, torch.tensor(proj[i::2]))
+else:
+    us_p = torch.tensor(geo['pfDetGamma'])
+    f = torch_1d_interp(us, us_p, torch.tensor(proj))
+# %%
+device= 'cuda:5'
+fov = 223
+A = FDK(geo=geo, nz=320, nx=512, fov=fov).to(device)
+# %%
+img = A.inv(torch.tensor(f[:,:,:], dtype=torch.float32).to(device))
+# %%
+ImageSliceViewer3D(img[0], [860,1260])
+# %%
+dicom_dir = os.path.join('/home/dpa/mount/D03/Staff/Zhiyang.Fu/DICOM_files',
+                        scan_type, scan_anatomy)
+dicom_file = series_uid + '_torchfdk'
+write_array_to_dicom( os.path.join(dicom_dir, dicom_file),
+                     np.flip(img[0].cpu().numpy(),axis=(1,2)),
+                    tilt=geo['fTiltAngle'],
+                    slice_thickness=geo['fSliceThickness'],
+                    mA=int(geo['fmA']),
+                    kvp=geo['fkVp'], fov=fov)
